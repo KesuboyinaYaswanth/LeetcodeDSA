bdfac56a0374d57fbea4e9d692a3926a5ac9f20e
Yaswanth Kesuboyina <69955464+KesuboyinaYaswanth@users.noreply.github.com>
2026-06-27 23:34:51 +0530

    Time: 32 ms (44.46%) | Memory: 156.1 MB (14.87%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n=nums.length;
+        int a=0;
+        int[] arr=new int[n-k+1];
+        Deque<Integer> q=new LinkedList<>();
+        for(int i=0;i<nums.length;i++){
+            while(!q.isEmpty() && q.peekFirst()<i-k+1){
+                q.removeFirst();
+            }
+            while(!q.isEmpty() && nums[q.peekLast()]<nums[i]){
+                q.removeLast();
+                
+            }
+            q.offer(i);
+            if(i>=k-1){
+                arr[a++]=nums[q.peekFirst()];
+            }
+
+        }
+        return arr;
+    }
+}
