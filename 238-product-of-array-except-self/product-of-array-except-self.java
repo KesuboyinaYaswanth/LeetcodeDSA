f4544d640d70d016edfc119d1ffbb9f655ae95da
Yaswanth Kesuboyina <69955464+KesuboyinaYaswanth@users.noreply.github.com>
2026-08-10 15:23:28 +0530

    Time: 5 ms (2.80%) | Memory: 64.9 MB (81.32%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -6,22 +6,16 @@ class Solution {
         for(int i=0;i<n;i++){
             int j=n-1-i;
             if(i==0){
-                prefpr[i]=nums[i];
-                sufpr[j]=nums[j];
+                prefpr[i]=1;
+                sufpr[j]=1;
             }else{
-                prefpr[i]=prefpr[i-1]*nums[i];
-                sufpr[j]=sufpr[j+1]*nums[j];
+                prefpr[i]=prefpr[i-1]*nums[i-1];
+                sufpr[j]=sufpr[j+1]*nums[j+1];
             }
         }
         int[] ans=new int[n];
         for(int i=0;i<n;i++){
-            if(i==0){
-                ans[i]=sufpr[i+1];
-            }else if(i==n-1){
-                ans[i]=prefpr[i-1];
-            }else{
-                ans[i]=sufpr[i+1]*prefpr[i-1];
-            }
+            ans[i]=sufpr[i]*prefpr[i];
         }
         return ans;
     }
