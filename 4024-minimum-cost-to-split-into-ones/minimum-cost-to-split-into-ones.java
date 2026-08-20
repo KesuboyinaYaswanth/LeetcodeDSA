ded8c0082b00b87547b082776153a8884313ca9e
Yaswanth Kesuboyina <69955464+KesuboyinaYaswanth@users.noreply.github.com>
2026-08-20 13:03:56 +0530

    Time: 122 ms (5.30%) | Memory: 43.1 MB (8.78%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.java
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.java
@@ -1,5 +1,22 @@
 class Solution {
+    
+    
+    public int solve(int n, int[] dp) {
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        if(n==1){
+            return 0;
+        }
+        int ans=Integer.MAX_VALUE;
+        for(int i=1;i<n;i++){
+            ans=Math.min(ans, i*(n-i)+(solve(i,dp)+solve(n-i,dp)));
+        }
+        return dp[n]=ans;
+    }
     public int minCost(int n) {
-        return n*(n-1)/2;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
     }
 }
